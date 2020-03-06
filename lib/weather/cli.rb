cd0f18f4fb7c472e406758e4faf721d4b1b5e0b1
ktalmage <ktalmage85@gmail.com>
2020-03-06 09:37:35 -0500

    cli update 3-6v1

--- lib/weather/cli.rb
+++ lib/weather/cli.rb
@@ -6,46 +6,54 @@
         puts "\nWelcome to Local Weather:\n"
         list_locations
         menu
-        
     end
             
 
     def list_locations
-     
-    Weather::API.get_locations.select.with_index {|name,i| puts "#{i + 1}. #{name[:name]}"}
-
+     Weather::API.get_locations.select.with_index {|name,i| puts "#{i + 1}. #{name[:name]}"}
     end
 
     def menu
 
-        @input = "" 
-        while @input != "exit"
+        input = "" 
+        while input != "exit"
             puts "\nPlease select the number of a city to receive weather:\n"
-            @input = gets.strip
-            weather
-            if @input.to_i < 0 || if @input.to_i > Weather::Location.all.size
-                puts "\nPlease enter a VALID number based on the list above\n"
+            input = gets.strip
+            if input.to_i - 1 <= Weather::Location.all.size
+                condition = Weather::Location.all[input.to_i - 1]
+                puts "The current weather conditions for #{condition.name} are:
+                Current Temperature: #{condition.temp}F
+                Feels like: #{condition.feels_like}F
+                Minimum Temperature: #{condition.temp_min}F
+                Maxium Temperarue: #{condition.temp_min}F
+                Humidity: #{condition.humidity}%
+                Wind Speed: #{condition.wind_speed} mph"
             
-            elsif @input == "exit"
-                puts "\nGoodbye.\n"
+            puts "To exit this program please type exit or select another number to continue:"
+
+            answer = gets.strip
+                if answer == "exit"
+                    puts "\nGoodbye.\n"
+                end
             end
         end
     end
-  end 
-
-    def weather
-        Weather::Location.all.find.with_index do |condition,i| @input.to_i - 1 == i
             
-            puts "The current weather conditions for #{condition.name} are:
-                    Current Temperature: #{condition.temp}F
-                    Feels like: #{condition.feels_like}F
-                    Minimum Temperature: #{condition.temp_min}F
-                    Maxium Temperarue: #{condition.temp_min}F
-                    Humidity: #{condition.humidity}%
-                    Wind Speed: #{condition.wind_speed} mph"
-        end
-        binding.pry
-    end
+            
+            
+            
+            
+                # if @input.to_i < 0 || if @input.to_i > Weather::Location.all.size
+            #     puts "\nPlease enter a VALID number based on the list above\n"
+            
+            # elsif @input == "exit"
+            #     puts "\nGoodbye.\n"
+            # end
+        
+
+    
+        
+    
        
 end
 
